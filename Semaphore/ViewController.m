c19b2a688d7581f1bf5aab6e0d4b95a6d0900232
“OldestDriver” <873074332@qq.com>
2017-08-21 16:33:46 +0800

    测试group

--- Semaphore/ViewController.m
+++ Semaphore/ViewController.m
@@ -22,6 +22,7 @@
 }
 
 - (IBAction)click:(id)sender {
+    NSLog(@"==semaphore==");
     //创建信号量
     dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
     //创建全局并行
@@ -57,6 +58,45 @@
         //所有任务完成
         NSLog(@"all end");
     });
+}
+
+- (IBAction)group:(id)sender {
+    NSLog(@"==group==");
+    dispatch_group_t group = dispatch_group_create();
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_group_enter(group);
+    dispatch_group_async(group, queue, ^{
+        NSLog(@"1start");
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(arc4random()%5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            dispatch_group_leave(group);
+            NSLog(@"1end");
+        });
+    });
+    dispatch_group_enter(group);
+    dispatch_group_async(group, queue, ^{
+        NSLog(@"2start");
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(arc4random()%5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            dispatch_group_leave(group);
+            NSLog(@"2end");
+        });
+    });
+    dispatch_group_enter(group);
+    dispatch_group_async(group, queue, ^{
+        NSLog(@"3start");
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(arc4random()%5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            dispatch_group_leave(group);
+            NSLog(@"3end");
+        });
+    });
+    
+    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
+        //所有任务完成
+        NSLog(@"all end");
+    });
+
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
